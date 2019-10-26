faf56f62ac1a09c1db204bb574921251c3a689dc
Neslihan Çekiç <32279951+neslihancekic@users.noreply.github.com>
2019-10-27 00:10:09 +0300

    Connected to ElephantSQL

--- dbinit.py
+++ dbinit.py
@@ -3,23 +3,32 @@ import sys
 
 import psycopg2 as dbapi2
 
+ 
 
 INIT_STATEMENTS = [
-    "CREATE TABLE IF NOT EXISTS DUMMY (NUM INTEGER)",
-    "INSERT INTO DUMMY VALUES (42)",
+
+    """
+       """
 ]
+
+#dsn = """user='postgres' password='docker'
+ #       host='localhost' port=5432 dbname='mydatabase'"""
 
 
 def initialize(url):
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
         for statement in INIT_STATEMENTS:
-            cursor.execute(statement)
+           cursor.execute(statement)
         cursor.close()
 
 
 if __name__ == "__main__":
-    url = os.getenv("DATABASE_URL")
+    print(44)
+else:
+    print("555")
+    url = os.getenv("python dbinit.py", "postgres://yqplkmdw:6ZAgWY_E0tcKhin3yUXcgbtQ0RJ7Sf43@manny.db.elephantsql.com:5432/yqplkmdw")
+    print(url)
     if url is None:
         print("Usage: DATABASE_URL=url python dbinit.py", file=sys.stderr)
         sys.exit(1)
--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 from flask import Flask
-
+import dbinit
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_page():
+    
+    print(35)
     return "Hello, world!"
+
 
 
 if __name__ == "__main__":
