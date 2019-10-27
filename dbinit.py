6abc8d86e975879e592b8a56cd31a5c15d7419ba
Neslihan Çekiç <32279951+neslihancekic@users.noreply.github.com>
2019-10-28 00:59:49 +0300

    Print table sample in website

--- dbinit.py
+++ dbinit.py
@@ -3,25 +3,39 @@ import sys
 
 import psycopg2 as dbapi2
 
- 
+class tvdb():
+    def __init__(self,idd="null",name="null",ep="null"):
+        self.idd=idd
+        self.name=name
+        self.ep=ep
 
 INIT_STATEMENTS = [
-
+    """SELECT * FROM tvseries;
     """
-       """
+   
 ]
 
 #dsn = """user='postgres' password='docker'
  #       host='localhost' port=5432 dbname='mydatabase'"""
-
+printf = tvdb()
 
 def initialize(url):
     with dbapi2.connect(url) as connection:
         cursor = connection.cursor()
         for statement in INIT_STATEMENTS:
-           cursor.execute(statement)
-        cursor.close()
+            cursor.execute(statement)
+            for row in cursor:
+                idd, title, ep = row
+                printf.name = title
+                printf.idd = idd
+                printf.ep = ep
 
+                
+        cursor.close()
+        
+
+def a():
+    return printf
 
 if __name__ == "__main__":
     print(44)
--- server.py
+++ server.py
@@ -1,15 +1,12 @@
-from flask import Flask
+from flask import Flask,render_template
 import dbinit
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home_page():
-    
-    print(35)
-    return "Hello, world!"
-
+    a=dbinit.a()
+    return render_template("index.html", lol2=a.name,lol=a.idd,lol3=a.ep)
 
 
 if __name__ == "__main__":
