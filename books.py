0abfc1c87906041c0d20cfc0e4055318e42e9349
muruvvetb <mbozkurtt76@gmail.com>
2019-10-28 22:18:02 +0300

    mur deneme

---
+++ books.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import dbinit as dbapi2
+
+url=dbapi2.url
+
+book_data = [
+    {'title': "Game of Thrones",
+     'year': 2011,
+     'score': 8.7,
+     'votes': 35027,
+     'channel':"HBO",
+     'language': "English",
+     'genre':"Fantastic",
+     'season': 8}
+]
