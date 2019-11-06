9329d6b33e36a8feefa36dafa8c725cca2a9c160
neslihancekic <neslihan.cekic@hotmail.com>
2019-11-07 00:30:14 +0300

    Update website

--- server.py
+++ server.py
@@ -9,7 +9,15 @@ def home_page():
     
     return render_template("index.html")
 
+@app.route("/home")
 
+def h_page():
+    return render_template("home.html")
+
+@app.route("/signup")
+
+def signup_page():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run()
